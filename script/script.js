f49505968f0af70c9cad50caf2b4465064d2277c
Ali-Chouchene <choucheneali9801@gmail.com>
2022-12-02 15:48:38 +0100

    create steps list

---
+++ script/script.js
@@ -0,0 +1,20 @@
+/*
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+*/
+
+
+/*
+1.Prendo l'elemento dall DOM.
+2.chiedo il nome all'utente (const).
+3.chiedi il suo cognome (const).
+4.chiedi il suo colore preferito (const).
+5.creo variabile numerica (stringa non va sommato).
+6.unisco i risultati per creare una password.
+7.stampo sulla pagina.
+
+
+//1.
+
