cdaeabba2674a7fe34a6bf22a514f6cd6879e065
Ali-Chouchene <choucheneali9801@gmail.com>
2022-12-02 16:13:51 +0100

    ended

--- script/script.js
+++ script/script.js
@@ -12,17 +12,28 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21
 3.chiedi il suo cognome (const).
 4.chiedi il suo colore preferito (const).
 5.creo variabile numerica (stringa non va sommato).
-6.unisco i risultati per creare una password.
-7.stampo sulla pagina.
+6.unisco i risultati per creare una password, e stampo sulla pagina..
 */
 
 //1.
 const targetResult = document.getElementById('result');
 
 //2.
-const firstName = prompt('Come ti chiami?', 'Ali');
+const firstName = prompt('Come ti chiami?', 'ali');
 ////console.log(firstName)
 
 //3.
-const lastName = prompt('Quale è il tuo cognome?', 'Chouchene')
+const lastName = prompt('Quale è il tuo cognome?', 'chouchene')
 ////console.log(lastName)
+
+//4.
+const color = prompt('Il tuo colore preferito?', 'verde')
+////console.log(color)
+
+//5.
+const number = Math.floor(Math.random() * 100);
+////console.log(number);
+
+//6.
+targetResult.innerText = `${firstName}${lastName}${color}${number}`;
+////console.log(targetResult)
